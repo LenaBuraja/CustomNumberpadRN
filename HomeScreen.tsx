59f4e9c050a21c57db128943c0a76579931f14a5
sunnycreature <sunnycreature@gmail.com>
2021-04-14 18:21:08 +0300

    feat: add mykeyboard

---
+++ HomeScreen.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
+
+export default function HomeScreen() {
+  const [goodQty, setGoodQty] = useState<string>('1');
+  const [goodQty2, setGoodQty2] = useState<string>('2');
+
+  const [isMyKeyboardVisible, setMyKeyboardVisible] = useState(false);
+
+  const onPress = (visibleMyKeyboard: boolean) => {
+    Keyboard.dismiss();
+    setMyKeyboardVisible(visibleMyKeyboard);
+  }
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <TouchableOpacity
+        style={isMyKeyboardVisible ? styles.inputFocus : styles.input}
+      >
+        <Text onPress={() => onPress(!isMyKeyboardVisible)}>
+          {goodQty}
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableWithoutFeedback onPress={() => onPress(false)}>
+        <View style={styles.inner}>
+          <TextInput style={styles.input} value={goodQty2} onFocus={() => setMyKeyboardVisible(false)}/>
+        </View>
+      </TouchableWithoutFeedback>
+
+      { isMyKeyboardVisible &&
+          <View style={{height: 200, width: '100%', backgroundColor: "#DDDDDD"}}>
+            <Text>My keyboard</Text>
+          </View>
+      }
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  inner: {
+    // padding: 24,
+    flex: 1,
+    justifyContent: "flex-start"
+  },
+  header: {
+    fontSize: 36,
+    marginBottom: 48
+  },
+  textInput: {
+    height: 40,
+    borderColor: "#000000",
+    borderBottomWidth: 1,
+    marginBottom: 36
+  },
+  btnContainer: {
+    backgroundColor: "white",
+    marginTop: 12
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    width: 100,
+  },
+  inputFocus: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    width: 100,
+    backgroundColor: 'red'
+  },
+});
